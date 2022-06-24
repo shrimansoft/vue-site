0b7b8b414fe4c54f6570fe155b1a299444eedc10
shrimansoft <shrimansoft@hotmail.com>
2022-06-24 13:21:51 +0530

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,6 @@ module.exports = {
   lang: 'en-US',
   title: 'Hello, VuePress!',
   description: 'This is my first VuePress site',
-  base: '/vue-site/'
+  base: '/vue-site/',
+  dest: '${sourceDir}/../dist'
 }
